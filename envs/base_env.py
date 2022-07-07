7e57edd7f1322794df4415592128a49e0f31eb24
isk03276 <isk03276@naver.com>
2022-07-07 12:57:19 +0900

    Change base observation space from image observation to vector observation

--- envs/base_env.py
+++ envs/base_env.py
@@ -1,3 +1,5 @@
+from typing import Tuple, List
+
 from utils.image import resize_image
 from utils.geometry import get_distance_between_two_pts
 
@@ -8,9 +10,15 @@ from pyrep import PyRep
 from pyrep.robots.arms.ur3 import UR3
 from pyrep.robots.end_effectors.robotiq85_gripper import Robotiq85Gripper
 from pyrep.objects.vision_sensor import VisionSensor
+from pyrep.objects.shape import Shape
+from pyrep.objects import Object
 
 
 class BaseEnv(gym.Env):
+    """
+    Base environment class for learning behavior of UR3 robot.
+    """
+
     def __init__(
         self, scene_file: str, use_arm_camera: bool = False, rendering: bool = True
     ):
@@ -20,6 +28,7 @@ class BaseEnv(gym.Env):
         self.env.launch(scene_file, headless=not rendering)
         self.env.start()
         self.arm = None
+        self.arm_base_link = None
         self.gripper = None
         self.tip = None
         self.third_view_camera = VisionSensor("kinect_rgb")
@@ -34,16 +43,64 @@ class BaseEnv(gym.Env):
         self.max_consecutive_visit_to_goal = 5
         self.cur_consecutive_visit_to_goal = 0
 
-        channel_num = 6 if use_arm_camera else 3
-        self.observation_space = Box(0, 255, (84, 84, channel_num))
+        self.observation_space = self._define_observation_space()
         self.action_space = Box(-1.0, 1, (7,))
 
     def _init_robot(self):
+        """
+        Initialize robot.
+        Assume that (robot arm) UR3 with (gripper) Robotiq85Gripper.
+        """
         self.arm = UR3()
+        self.arm_base_link = Shape("UR3_link2")
         self.gripper = Robotiq85Gripper()
         self.tip = self.arm.get_tip()
 
-    def get_obs(self):
+    def _define_observation_space(self) -> Box:
+        """
+        Define/Get observation space.
+        Returns:
+            (Box) : defined observation space
+        """
+        raise NotImplementedError
+
+    def get_obs(self) -> np.ndarray:
+        """
+        Get agent's observation.
+        Returns:
+            (np.ndarray) : agent's observation
+        """
+        raise NotImplementedError
+
+    def get_reward(self) -> float:
+        """
+        Get reward for state/action.
+        We currently assume reward for state.
+        Returns:
+            (float) : reward
+        """
+        raise NotImplementedError
+
+    def reset_objects(self):
+        """
+        Reset objects in env. (ex. target_object)
+        """
+        raise NotImplementedError
+
+    def is_goal_state(self) -> bool:
+        """
+        Whether the current state is a goal state or not.
+        Returns :
+            (bool) : if current state is a goal state, then return True
+        """
+        raise NotImplementedError
+
+    def render(self) -> np.ndarray:
+        """
+        Get image observation from cameras.
+        Returns:
+            np.ndarray: image observation
+        """
         width = self.observation_space.shape[0]
         height = self.observation_space.shape[1]
         obs = self.third_view_camera.capture_rgb()
@@ -53,30 +110,44 @@ class BaseEnv(gym.Env):
         obs = resize_image(obs, width, height)
         return obs
 
-    def get_reward(self):
-        raise NotImplementedError
-
-    def reset_objects(self):
-        raise NotImplementedError
-
-    def is_goal_state(self):
-        raise NotImplementedError
-
-    def get_done_and_info(self):
+    def get_done_and_info(self) -> Tuple[bool, dict]:
+        """
+        Get done and info.
+        The done indicates whether the episode is over.
+        The info contains several informations such as whether the episode is successed.
+        Returns :
+            (bool) : done
+            (dict) : info
+        """
         is_success = self.is_success()
         return self.time_over() | is_success, {"success": is_success}
 
-    def is_success(self):
+    def is_success(self) -> bool:
+        """
+        Whether the episode is successed.
+        Returns:
+            (bool) : if the episode is successed, then return True
+        """
         if self.is_goal_state():
             self.cur_consecutive_visit_to_goal += 1
         else:
             self.cur_consecutive_visit_to_goal = 0
         return self.cur_consecutive_visit_to_goal >= self.max_consecutive_visit_to_goal
 
-    def time_over(self):
+    def time_over(self) -> bool:
+        """
+        Whether the episode is progressing beyond the set maximum time step. 
+        Returns:
+            (bool) : if timestep is overed, then return True
+        """
         return True if self.current_time_step >= self.max_time_step else False
 
-    def reset(self):
+    def reset(self) -> np.ndarray:
+        """
+        Reset env and Start new episode.
+        Returns:
+            (np.ndarray) : initial obs
+        """
         self.current_time_step = 0
         self.cur_consecutive_visit_to_goal = 0
         self.env.stop()
@@ -87,7 +158,18 @@ class BaseEnv(gym.Env):
         self.env.step()
         return self.get_obs()
 
-    def step(self, action):
+    def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool, dict]:
+        """
+        Execute the action and Observe the new obs, reward, etc.
+        Args:
+            action (np.ndarray): the action decided by the agent
+
+        Returns:
+            (np.ndarray) : new obs
+            (float) : reward
+            (bool) : done
+            (dict) : info
+        """
         self.current_time_step += 1
         arm_control = action[:-1]
         gripper_control = action[-1]
@@ -99,8 +181,53 @@ class BaseEnv(gym.Env):
         return self.get_obs(), self.get_reward(), done, info
 
     def close(self):
+        """
+        Close the env.
+        """
         self.env.stop()
         self.env.shutdown()
 
-    def get_distance_from_tip(self, object_position):
+    def get_distance_from_tip(self, object_position: np.ndarray) -> float:
+        """
+        Get distance between tip and target object
+        Args:
+            object_position (np.ndarray): position of the target object
+
+        Returns:
+            (float): distance
+        """
         return get_distance_between_two_pts(self.tip.get_position(), object_position)
+
+    def get_robot_state(self) -> List[float]:
+        """
+        Get the robot state.
+        The robot state contains the robot arm state and the gripper state.
+        The robot arm state contains positions of arm joints and velocities of arm joints.
+        The gripper state contains mean of open amount of gripper and mean of velocities of gripper joints.
+        Returns:
+            (list) : robot state
+        """
+        arm_joint_positions = self.arm.get_joint_positions()
+        arm_joint_velocities = self.arm.get_joint_velocities()
+        gripper_positions = self.gripper.get_open_amount()
+        gripper_velocities = self.gripper.get_joint_velocities()
+        gripper_position_mean = sum(gripper_positions) / len(gripper_positions)
+        gripper_velocity_mean = sum(gripper_velocities) / len(gripper_velocities)
+        robot_state = arm_joint_positions
+        robot_state.extend(arm_joint_velocities)
+        robot_state.append(gripper_position_mean)
+        robot_state.append(gripper_velocity_mean)
+        return robot_state
+
+    def get_object_position_relative_to_base_link(
+        self, target_object: Object
+    ) -> np.ndarray:
+        """
+        Get relative positoin of the target object to robot arm base link.
+        Args:
+            target_object (Object): target object
+
+        Returns:
+            (np.ndarray): position array containing x, y, z
+        """
+        return target_object.get_position(relative_to=self.arm_base_link)
--- envs/reach/reach_env.py
+++ envs/reach/reach_env.py
@@ -1,12 +1,19 @@
 import random
 import math
+from typing import overload
 
 from envs.base_env import BaseEnv
 
+import numpy as np
 from pyrep.objects.shape import Shape
+from gym.spaces import Box
 
 
 class ReachEnv(BaseEnv):
+    """
+    Reach env class.
+    """
+
     def __init__(
         self,
         scene_file="envs/reach/reach.ttt",
@@ -21,19 +28,51 @@ class ReachEnv(BaseEnv):
 
         self.reach_threshold = 0.05
 
-    def get_reward(self):
+    def _define_observation_space(self) -> Box:
+        """
+        Define/Get observation space.
+        """
+        observation_space = Box(float("-inf"), float("inf"), (17,))
+        return observation_space
+
+    def get_obs(self) -> np.ndarray:
+        """
+        Get agent's observation.
+        The observation contains robot state and a relative position of target object
+        Returns:
+            (np.ndarray) : agent's observation
+        """
+        obs = self.get_robot_state()
+        target_realtive_position = self.get_object_position_relative_to_base_link(
+            self.target
+        )
+        obs.extend(target_realtive_position)
+        return np.array(obs)
+
+    def get_reward(self) -> float:
+        """
+        This reward function is based on the distance between target object and tip.
+        Returns:
+            (float) : reward
+        """
         distance_between_tip_and_target = self.get_distance_from_tip(
             self.target.get_position()
         )
         return -math.log10(distance_between_tip_and_target / 10 + 1)
 
     def reset_objects(self):
+        """
+        Reset a target object.
+        """
         random_point_x = random.uniform(self.target_x_range[0], self.target_x_range[1])
         random_point_y = random.uniform(self.target_y_range[0], self.target_y_range[1])
         random_point_z = random.uniform(self.target_z_range[0], self.target_z_range[1])
         self.target.set_position([random_point_x, random_point_y, random_point_z])
 
-    def is_goal_state(self):
+    def is_goal_state(self) -> bool:
+        """
+        If the target object and the tip are close, it is considered as a goal state.
+        """
         distance_between_tip_and_target = self.get_distance_from_tip(
             self.target.get_position()
         )
